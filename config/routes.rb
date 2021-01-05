02fec38eddef354ab861d750f16dbeb7878375c9
Ikuto Hongu <1999ikutochan@gmail.com>
2021-01-05 18:18:49 +0900

    fix Sinup and made login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+
   root 'static_pages#home'
  
  
