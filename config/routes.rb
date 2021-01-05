8f0e61a788d031ad83ac4581f3368f40e88aae6a
Ikuto7080 <76934531+Ikuto7080@users.noreply.github.com>
2021-01-05 18:42:57 +0900

    Signup (#2)
    * Signup
    
    * fix signup
    
    * fix Sinup and made login function
    
    * fixed Signup
    
    Co-authored-by: Ikuto Hongu <1999ikutochan@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+
   root 'static_pages#home'
  
  
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  resources :users
+  get '/signup', to: 'users#new'
  
  end
