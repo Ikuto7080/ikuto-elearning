35500f0f24603aa3e4dbe067fa9fbfc9b634cf7a
Ikuto7080 <76934531+Ikuto7080@users.noreply.github.com>
2021-01-06 18:07:35 +0900

    Update User (#5)
    * made the update
    
    * done update
    
    * create update controller
    
    Co-authored-by: Ikuto Hongu <1999ikutochan@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :only_loggedin_users, only: [:edit, :update]
+
+  def index
+   # @users = User.all
+   @users = User.paginate(page:params[:page], per_page: 10 )
+  end
+
   def new
     @user = User.new
   end
@@ -7,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Saved successfully!"
-      redirect_to @user
+      redirect_to login_url
     else
       flash[:danger] = "Invalid content. Try again."
       render 'new'
@@ -18,8 +25,39 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Saved successfully!"
+      redirect_to @user
+    else
+      flash[:danger] = "Invalid content. Try again."
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id].destroy)
+    redirect_to users_url
+  end
+
   private
   def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def only_loggedin_users
+    redirect_to login_url unless logged_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+
+    redirect_to(root_url) unless
+    current_user?(@user)
   end
 end
