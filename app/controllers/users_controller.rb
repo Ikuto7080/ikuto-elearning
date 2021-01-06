3ede1164e1a8efc29739dd53bb8dd2fad482509e
Ikuto Hongu <1999ikutochan@gmail.com>
2021-01-06 16:45:37 +0900

    made the update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :only_loggedin_users, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -7,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Saved successfully!"
-      redirect_to @user
+      redirect_to login_url
     else
       flash[:danger] = "Invalid content. Try again."
       render 'new'
@@ -18,8 +24,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def only_loggedin_users
+    redirect_to login_url unless logged_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+
+    redirect_to(root_url) unless
+    current_user?(@user)
   end
 end
