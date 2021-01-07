74cbfa915dc852c208e2357b1488a7523209403a
Ikuto Hongu <1999ikutochan@gmail.com>
2021-01-07 17:07:37 +0900

    fixed logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   
   resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: 'sessions#new'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   root 'static_pages#home'
  
